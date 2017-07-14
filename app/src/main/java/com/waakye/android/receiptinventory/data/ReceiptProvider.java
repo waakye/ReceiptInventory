2395d1236597a9449da4ed57699dd9419fd91fd3
waakye <dailyomnivore@gmail.com>
2017-07-14 14:21:35 -0700

    Implement ContentProvider query() method

--- app/src/main/java/com/waakye/android/receiptinventory/data/ReceiptProvider.java
+++ app/src/main/java/com/waakye/android/receiptinventory/data/ReceiptProvider.java
@@ -1,9 +1,11 @@
 package com.waakye.android.receiptinventory.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -47,7 +49,7 @@ public class ReceiptProvider extends ContentProvider {
         sUriMatcher.addURI(ReceiptContract.CONTENT_AUTHORITY, ReceiptContract.PATH_RECEIPTS + "/#",
                 RECEIPT_ID);
     }
-    
+
     /** Database helper object */
     private ReceiptDbHelper mDbHelper;
 
@@ -59,8 +61,45 @@ public class ReceiptProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
-        return null;
+                        String sortOrder){
+        // Get readable database
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = sUriMatcher.match(uri);
+        switch(match){
+            case RECEIPTS:
+                // For the RECEIPTS code, query the receipts table directly with the given
+                // projection, selection, selection arguments, and sort order.  The cursor could
+                // contain multiple rows of the receipts table.
+                cursor = database.query(ReceiptContract.ReceiptEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            case RECEIPT_ID:
+                // For the RECEIPT_ID code, extract out the ID from the URI.
+                // For an example URI such as
+                // "content://com.waakye.android.receiptinventory/receipts/3", the selection will
+                // be "_id=?" and the selection argument will be a String array containing the
+                // actual ID of 3 in this case.
+                //
+                // For every "?" in the selection, we need to have an element in that selection
+                // arguments that will fill in the "?".  Since we have 1 question mark in the
+                // selection, we have 1 String in the selection arguments' String array.
+                selection = ReceiptContract.ReceiptEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                // This will perform a query on the receipts table where the _id equals 3 to
+                // return a Cursor containing that row of the table.
+                cursor = database.query(ReceiptContract.ReceiptEntry.TABLE_NAME, projection,
+                        selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     @Override
