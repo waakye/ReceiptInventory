b46f084abd0863dd3449738ca7874767c8ebc276
waakye <dailyomnivore@gmail.com>
2017-07-17 08:59:14 -0700

    Modify ContentProvider so Loader refreshes data automatically

--- app/src/main/java/com/waakye/android/receiptinventory/data/ReceiptProvider.java
+++ app/src/main/java/com/waakye/android/receiptinventory/data/ReceiptProvider.java
@@ -103,6 +103,11 @@ public class ReceiptProvider extends ContentProvider {
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
+
+        // Set notification URI on the Cursor
+        // so we know what content URI the Cursor was created for.  If the data at this URI changes,
+        // then we know we need to update the Cursor
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
         return cursor;
     }
 
@@ -159,6 +164,9 @@ public class ReceiptProvider extends ContentProvider {
             Log.e(LOG_TAG, "Failed to insert row for " + uri);
             return null;
         }
+
+        // Notify all listeners that the data has changed for the receipt content URI
+        getContext().getContentResolver().notifyChange(uri, null);
 
         // Return the new URI with the ID (of the newly inserted row) appended at the end
         return ContentUris.withAppendedId(uri, id);
@@ -236,9 +244,18 @@ public class ReceiptProvider extends ContentProvider {
         // Otherwise, get writeable database to update the data
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
-        // Returns the number of database rows affected by the update statement
-        return database.update(ReceiptContract.ReceiptEntry.TABLE_NAME, values, selection,
-                selectionArgs);
+        // Perform the update on the database and get the number of rows affected
+        int rowsUpdated = database.update(ReceiptContract.ReceiptEntry.TABLE_NAME, values,
+                selection, selectionArgs);
+
+        // If 1 or more rows were updated, then notify all listeners that the data at the given URI
+        // has changed.
+        if (rowsUpdated != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of rows updated
+        return rowsUpdated;
     }
 
     @Override
@@ -260,22 +277,35 @@ public class ReceiptProvider extends ContentProvider {
         // Get writeable database
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
+        // Track the number of rows that were deleted
+        int rowsDeleted;
+
         final int match = sUriMatcher.match(uri);
         switch(match) {
             case RECEIPTS:
                 // Delete all rows that match the selection and selection args
-                return database.delete(ReceiptContract.ReceiptEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(ReceiptContract.ReceiptEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
             case RECEIPT_ID:
                 // Delete a single row given by the ID in the URI
                 selection = ReceiptContract.ReceiptEntry._ID + "=?";
                 selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
-                return database.delete(ReceiptContract.ReceiptEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(ReceiptContract.ReceiptEntry.TABLE_NAME, selection,
+                        selectionArgs);
+                break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
 
+        // If 1 or more rows were deleted, then notify all listeners that the data at the given URI
+        // has changed.
+        if(rowsDeleted != 0){
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // Return the number of rows deleted
+        return rowsDeleted;
+
     }
-
-
-
 }
