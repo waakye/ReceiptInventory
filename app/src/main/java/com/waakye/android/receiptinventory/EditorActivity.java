efd6097c7d56a1223f672279bcfe83f26843de37
waakye <dailyomnivore@gmail.com>
2017-07-21 11:20:23 -0700

    Hide delete menu option for new receipts

--- app/src/main/java/com/waakye/android/receiptinventory/EditorActivity.java
+++ app/src/main/java/com/waakye/android/receiptinventory/EditorActivity.java
@@ -119,6 +119,10 @@ public class EditorActivity extends AppCompatActivity
         if (mCurrentReceiptUri == null){
             // This is a new receipt, so change the app bar to say "Add a Receipt"
             setTitle(getString(R.string.editor_activity_title_new_receipt));
+
+            // Invalidate the options menu, so the "Delete" menu option can be hidden
+            // (It doesn't make sense to delete a receipt that hasn't been created yet.)
+            invalidateOptionsMenu();
         } else {
             // Otherwise, this an existing receipt, so change app bar to say "Edit Receipt"
             setTitle(getString(R.string.editor_activity_title_edit_receipt));
@@ -282,6 +286,22 @@ public class EditorActivity extends AppCompatActivity
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    /**
+     * This method is called after invalidateOptionsMenu(), so that the menu can be updated
+     * (some menu items can be hidden or made visible).
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new receipt, hide the "Delete" menu item.
+        if(mCurrentReceiptUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+
         return true;
     }
 
