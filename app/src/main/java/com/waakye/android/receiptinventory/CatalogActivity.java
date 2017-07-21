5dbce48caa39214684153822190c7f3055f5604e
waakye <dailyomnivore@gmail.com>
2017-07-21 11:37:03 -0700

    Delete all receipts from catalog menu item

--- app/src/main/java/com/waakye/android/receiptinventory/CatalogActivity.java
+++ app/src/main/java/com/waakye/android/receiptinventory/CatalogActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -104,6 +105,15 @@ public class CatalogActivity extends AppCompatActivity
         Uri newUri = getContentResolver().insert(ReceiptContract.ReceiptEntry.CONTENT_URI, values);
     }
 
+    /**
+     * Helper method to delete all receipts in the database
+     */
+    private void deleteAllReceipts(){
+        int rowsDeleted = getContentResolver().delete(ReceiptContract.ReceiptEntry.CONTENT_URI,
+                null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from receipts database");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file
@@ -122,7 +132,7 @@ public class CatalogActivity extends AppCompatActivity
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+                deleteAllReceipts();
                 return true;
         }
 
